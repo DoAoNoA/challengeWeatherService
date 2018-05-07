916e501d1e3aedc7d8411e095d74a8c264f7ec25
D4n1Ela <dana@correoFalso.com>
2018-05-07 11:53:46 -0300

    server: user.js: Comento la request a API de Yahoo y dejo activa la de json-server.

--- weather-service/routes/users.js
+++ weather-service/routes/users.js
@@ -14,8 +14,8 @@ router.get('/users/:id', (req, res, next) => {
             const promises = myCities.map(async city => {
                 const myCity = city.name; 
                 const myRegion = city.region; 
-                //const response = await axios.get(`http://localhost:3000/${myCity}`);
-                const response = await axios.get(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${myCity}%2C%20${myRegion}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`);
+                const response = await axios.get(`http://localhost:3000/${myCity}`);
+                //const response = await axios.get(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${myCity}%2C%20${myRegion}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`);
                 return response.data
             })
 
